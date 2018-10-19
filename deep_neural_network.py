40e579e0150de2d7512775f1a7c84a2a59dece3c
dlofstrom <lofstrom.daniel@gmail.com>
2018-10-19 10:20:47 +0200

    Removed bias array

--- deep_neural_network.py
+++ deep_neural_network.py
@@ -5,15 +5,11 @@ layer_sizes = (784, 16, 16, 10)
 
 # Neurons
 # N arrays of size 1 x n_L
-neurons = tuple([np.zeros((y,1)) for y in layer_sizes])
+neurons = tuple([np.concatenate((np.zeros((y,1)),[[1]]),axis=0) for y in layer_sizes])
 
 # Weights
 # N-1 matrices of size  n_L-1 x n_L (skipping first layer)
-weights = tuple([np.zeros((y,x)) for x,y in zip(layer_sizes[:-1],layer_sizes[1:])])
-
-# Biases
-# N-1 arrays of size 1 x n_L (skipping first layer)
-biases = tuple([np.zeros((y,1)) for y in layer_sizes[1:]])
+weights = tuple([np.zeros((y,x+1)) for x,y in zip(layer_sizes[:-1],layer_sizes[1:])])
 
 
 # Calculate sigmoid function for input
@@ -29,18 +25,17 @@ def sigmoid(Z):
 def forward_propagate(X):
     global neurons
     global weights
-    global biases
-
+    
     # Set first layer to input
     # Force convert list or array to single column
     # [True] replaces every element but keeps the original object
-    neurons[0][True] = np.transpose(np.matrix(X).flatten())
+    neurons[0][:-1] = np.transpose(np.matrix(X).flatten())
 
     # Go through every layer and calculate neuron activation
     for i in range(len(neurons)-1):
-        neurons[i+1][True] = np.matmul(weights[i], sigmoid(neurons[i])) + biases[i]
+        neurons[i+1][:-1] = np.matmul(weights[i], sigmoid(neurons[i]))
 
-    return sigmoid(neurons[-1])
+    return sigmoid(neurons[-1][:-1])
 
 
 # Calculaye derivate of sigmoid
@@ -64,14 +59,12 @@ def cost_prime(AL, Y):
 def backpropagate(Xs, Ys, alpha):
     global neurons
     global weights
-    global biases
-
+    
     if len(Xs) != len(Ys):
         print('Xs and Ys not same size')
     
     # Error lists filled with individual gradients
     dCdWs = []
-    dCdBs = []
     costs = []
     
     while Xs:
@@ -87,8 +80,8 @@ def backpropagate(Xs, Ys, alpha):
         C = cost(Yh, Y)
         
         # Errors for this test case
-        dCdW = tuple([np.zeros((y,x)) for x,y in zip(layer_sizes[:-1],layer_sizes[1:])])
-        dCdB = tuple([np.zeros((y,1)) for y in layer_sizes[1:]])
+        # TODO: Automatically the same format
+        dCdW = tuple([np.zeros((y,x+1)) for x,y in zip(layer_sizes[:-1],layer_sizes[1:])])
         
         # Keep chain rule tail
         # Start: dC0/dAL
@@ -100,20 +93,15 @@ def backpropagate(Xs, Ys, alpha):
         for i in reversed(range(len(layer_sizes)-1)):
             # Append chain rule
             # dAL/dZL = sigmoid prime
-            Z = neurons[i+1] # nLx1
+            Z = neurons[i+1][:-1] # nLx1
             dCdZLx = np.multiply(sigmoid_prime(Z), dCdZLx) # nLx1
-            
-            
-            # Calculate b gradient
-            dCdBL = dCdB[i]
-            dCdBL[True] = dCdZLx
             
             # Calculate w gradient
             dCdWL = dCdW[i] # nL-1xnL
             #print("dCdWL " + str(dCdWL.shape))
             # dZL/dWL = AL-1 = sigmoid(ZL-1)
-            ZLm1 = neurons[i]
-            dZdWL = sigmoid(ZLm1) # nL-1x1
+            ZLm1 = neurons[i][:-1]
+            dZdWL = np.concatenate((sigmoid(ZLm1),[[1]]),axis=0) # (nL-1+1)x1
             #print("dZdWL " + str(np.transpose(dZdWL).shape) + " dCdZLxT " + str(dCdZLx.shape))
             #print("dZdWL")
             #print(dZdWL)
@@ -124,25 +112,21 @@ def backpropagate(Xs, Ys, alpha):
             
             # Append chain rule
             # dZL/dAL-1 = sigmoid prime
-            dZdALm1 = weights[i] # nLxnL-1 TODO: may have to transpose
-            dCdZLx = np.matmul(np.transpose(dZdALm1),dCdZLx) # nL-1x1
+            dZdALm1 = weights[i][:,:-1] # nLxnL-1 TODO: may have to transpose
+            dCdZLx = np.matmul(np.transpose(dZdALm1),dCdZLx) # (nL-1+1)x1
             
         # Now append this to dCdWs and dCdBs
         dCdWs.append(dCdW)
-        dCdBs.append(dCdB)
         costs.append(np.sum(C))
 
     # Calculate average gradient
     dCdW = tuple([np.mean(x,axis=0) for x in zip(*dCdWs)])
-    dCdB = tuple([np.mean(x,axis=0) for x in zip(*dCdBs)])
     C = np.mean(costs)
     
     # Adjust weights and biases
     for W,dW in zip(weights,dCdW):
         W[True] = W - alpha*dW
-    for B,dB in zip(biases,dCdB):
-        B[True] = B - alpha*dB
-        
+            
     return C
 
 # Train network
@@ -150,9 +134,7 @@ def backpropagate(Xs, Ys, alpha):
 def train():
     global neurons
     global weights
-    global biases
-
-    
+        
     X = np.random.rand(layer_sizes[0])
     Y = np.array([1]+[0 for i in range(layer_sizes[-1]-1)])
     n = 10
@@ -163,7 +145,6 @@ def train():
     # Start with random weights and biases
     # Random number matrix of same shape
     weights = tuple([np.random.randn(*w.shape) for w in weights])
-    biases = tuple([np.random.randn(*b.shape) for b in biases])
 
     for i in range(1000):
         C = backpropagate(list(Xs), list(Ys), 0.1)
